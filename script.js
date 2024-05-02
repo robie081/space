6036b88e9d9537657a47222d0119e32e4b218ee7
robie081 <robert.lantus@icloud.com>
2024-05-02 12:27:25 +0300

    new timeless wisdom

--- script.js
+++ script.js
@@ -1,24 +1,69 @@
 
-const responsiveNavbar = () => {
-    const primaryNav = document.querySelector('.primary-navigation');
-    const navToggle = document.querySelector('.mobile-nav-toggle');
+const hamburgerButton = document.querySelector('.nav-hamburger-button-root');
+const hamburger = document.querySelector('.nav-hamburger-button-inner');
 
-    navToggle.addEventListener('click', () => {
-        const visibility = primaryNav.getAttribute('data-visible');
-        // console.log(visibility);
-        if (visibility === 'false') {
-            primaryNav.setAttribute('data-visible', 'true');
-            navToggle.setAttribute('aria-expanded', 'true');
-        } else {
-            primaryNav.setAttribute('data-visible', 'false');
-            navToggle.setAttribute('aria-expanded', 'false');
-        }
-    });
+// const primaryNav = document.querySelector('.primary-navigation');
+const nav = document.querySelector('nav');
 
-    // Toggle after developedByEd
-    // navToggle.addEventListener('click', () => {
-    //     primaryNav.classList.toggle('nav-active');
-    // });
-}
+// console.log(hamburger);
+hamburgerButton.addEventListener('click', () => {
 
-responsiveNavbar();
+    hamburger.classList.toggle('isExpanded');
+    
+    console.log('isExpanded');
+
+    // console.log(hamburger.classList.toggle('isExpanded'));
+
+    // const visibility = hamburger.getAttribute('aria-expanded');
+    // console.log(visibility, typeof visibility);
+    // if (visibility === 'false') {
+
+    // }
+});
+
+// Precious -- Hover over pseudo elements like ::after and ::before
+
+hamburgerButton.addEventListener('mouseover', () => {
+    // hamburger.classList.toggle('isHovered');
+    hamburger.classList.add('isHovered');
+    console.log('mouse over');
+});
+
+hamburgerButton.addEventListener('mouseout', () => {
+    // hamburger.classList.toggle('isHovered');
+    hamburger.classList.remove('isHovered');
+    console.log('mouse out');
+});
+
+hamburgerButton.addEventListener('click', () => {
+
+    // primaryNav.classList.toggle('active');
+    nav.classList.toggle('active');
+
+});
+
+// Original
+
+// const responsiveNavbar = () => {
+//     const primaryNav = document.querySelector('.primary-navigation');
+//     const navToggle = document.querySelector('.mobile-nav-toggle');
+
+//     navToggle.addEventListener('click', () => {
+//         const visibility = primaryNav.getAttribute('data-visible');
+//         // console.log(visibility);
+//         if (visibility === 'false') {
+//             primaryNav.setAttribute('data-visible', 'true');
+//             navToggle.setAttribute('aria-expanded', 'true');
+//         } else {
+//             primaryNav.setAttribute('data-visible', 'false');
+//             navToggle.setAttribute('aria-expanded', 'false');
+//         }
+//     });
+
+//     // Toggle after developedByEd
+//     // navToggle.addEventListener('click', () => {
+//     //     primaryNav.classList.toggle('nav-active');
+//     // });
+// }
+
+// responsiveNavbar();
