1a40daabd1b71838d2a0eca7bd567c8d7c90c1cd
robie081 <robert.lantus@icloud.com>
2024-05-08 13:10:47 +0300

    + poetry

--- script.js
+++ script.js
@@ -10,15 +10,7 @@ hamburgerButton.addEventListener('click', () => {
 
     hamburger.classList.toggle('isExpanded');
     
-    console.log('isExpanded');
-
-    // console.log(hamburger.classList.toggle('isExpanded'));
-
-    // const visibility = hamburger.getAttribute('aria-expanded');
-    // console.log(visibility, typeof visibility);
-    // if (visibility === 'false') {
-
-    // }
+    // console.log('isExpanded');
 });
 
 // Precious -- Hover over pseudo elements like ::after and ::before
@@ -26,13 +18,13 @@ hamburgerButton.addEventListener('click', () => {
 hamburgerButton.addEventListener('mouseover', () => {
     // hamburger.classList.toggle('isHovered');
     hamburger.classList.add('isHovered');
-    console.log('mouse over');
+    // console.log('mouse over');
 });
 
 hamburgerButton.addEventListener('mouseout', () => {
     // hamburger.classList.toggle('isHovered');
     hamburger.classList.remove('isHovered');
-    console.log('mouse out');
+    // console.log('mouse out');
 });
 
 hamburgerButton.addEventListener('click', () => {
@@ -41,29 +33,3 @@ hamburgerButton.addEventListener('click', () => {
     nav.classList.toggle('active');
 
 });
-
-// Original
-
-// const responsiveNavbar = () => {
-//     const primaryNav = document.querySelector('.primary-navigation');
-//     const navToggle = document.querySelector('.mobile-nav-toggle');
-
-//     navToggle.addEventListener('click', () => {
-//         const visibility = primaryNav.getAttribute('data-visible');
-//         // console.log(visibility);
-//         if (visibility === 'false') {
-//             primaryNav.setAttribute('data-visible', 'true');
-//             navToggle.setAttribute('aria-expanded', 'true');
-//         } else {
-//             primaryNav.setAttribute('data-visible', 'false');
-//             navToggle.setAttribute('aria-expanded', 'false');
-//         }
-//     });
-
-//     // Toggle after developedByEd
-//     // navToggle.addEventListener('click', () => {
-//     //     primaryNav.classList.toggle('nav-active');
-//     // });
-// }
-
-// responsiveNavbar();
