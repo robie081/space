454de6e97710c73ce0bb61d121df2435e2a5a5bf
robie081 <robert.lantus@icloud.com>
2024-05-18 18:35:21 +0300

    about mobile 2

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ hamburgerButton.addEventListener('click', () => {
 
 // Precious -- Hover over pseudo elements like ::after and ::before
 
-hamburgerButton.addEventListener('mouseover', () => {
-    // hamburger.classList.toggle('isHovered');
-    hamburger.classList.add('isHovered');
-    // console.log('mouse over');
-});
+// hamburgerButton.addEventListener('mouseover', () => {
+//     // hamburger.classList.toggle('isHovered');
+//     hamburger.classList.add('isHovered');
+//     // console.log('mouse over');
+// });
 
-hamburgerButton.addEventListener('mouseout', () => {
-    // hamburger.classList.toggle('isHovered');
-    hamburger.classList.remove('isHovered');
-    // console.log('mouse out');
-});
+// hamburgerButton.addEventListener('mouseout', () => {
+//     // hamburger.classList.toggle('isHovered');
+//     hamburger.classList.remove('isHovered');
+//     // console.log('mouse out');
+// });
 
 hamburgerButton.addEventListener('click', () => {
 
@@ -39,18 +39,20 @@ const dayNight = document.querySelector('.day-night');
 const solSvg = document.getElementById('sol-svg');
 const body = document.querySelector('body');
 
-const linkedin = document.querySelector('.linkedin');
-const github = document.querySelector('.github');
+// const linkedin = document.querySelector('.linkedin');
+// const github = document.querySelector('.github');
+
+const ul = document.getElementById('social');
+// console.log(ul);
+const li = ul.querySelectorAll('.social');
+// console.log(li);
+
+
 
 dayNight.addEventListener('click', () => {
-    body.classList.toggle('dark');
-    hamburger.classList.toggle('night');
+    body.classList.toggle('light');
+    hamburger.classList.toggle('dark');
 
-    // hamburger.style.color = '#f9f4da';
-    // hamburger.style.backgroundColor = '#0f0d0e';
-    // solSvg.setAttribute('fill', '#ccc');
-    // console.log(solSvg.style.fill);
-    
     if (solSvg.style.fill === '') {
         // solSvg.style.fill = '#f9f4da';
         solSvg.style.fill = '#111827';
@@ -58,6 +60,16 @@ dayNight.addEventListener('click', () => {
         solSvg.style.fill = '';
     }
     
-    linkedin.classList.toggle('darky');
-    github.classList.toggle('darky');
+    // linkedin.classList.toggle('darky');
+    // github.classList.toggle('darky');
+
+    // li.forEach(item => {
+    //     let a = item.getElementsByTagName('a')[0];
+    //     a.classList.toggle('darky');
+    // });
+
+    for (let item of li) {
+        let a = item.getElementsByTagName('a')[0];
+        a.classList.toggle('darky');
+    }
 });
